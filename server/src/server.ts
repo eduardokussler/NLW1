edd013d1e12ac6508eacfcba7ac4cf8d8fec5ff6
eduardokussler <eduardoeugeniok@gmail.com>
2020-06-03 14:44:09 -0300

    Backend completed

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import routes from './routes';
+
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem dos usuários!');
-  return response.json([
-    "Eduardo",
-    "Pedro",
-    "Lucas"
-  ]);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
